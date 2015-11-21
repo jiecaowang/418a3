8b59dc112b755364c89d28b0f0085d2a2a58fbe7
Coding <jiecao.wang@gmail.com>
2015-11-21 16:06:10 -0500

    done unit square intersect

--- a3/raytracerLinux/scene_object.cpp
+++ a3/raytracerLinux/scene_object.cpp
@@ -25,7 +25,32 @@ bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	//
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
+	// 
+	// Theory: http://www.cs.toronto.edu/~liviu/CSC418_Tutorials_F2013/T7-2.jpg
+	// Code: http://www.cs.toronto.edu/~liviu/CSC418_Tutorials_F2013/T7-3.jpg
+	
+	Ray3D rayInModel;
+	rayInModel.origin = worldToModel * ray.origin;
+	rayInModel.dir = worldToModel * ray.dir;
+	double t = -(rayInModel.origin[2])/rayInModel.dir[2];
+	if(t <= 0){
+		return false;
+	}
+	double xInModel = rayInModel.origin[0] + t * rayInModel.dir[0];
+	double yInModel = rayInModel.origin[1] + t * rayInModel.dir[1];
 
+	if(-0.5 <= xInModel && xInModel <= 0.5 && -0.5 <= yInModel && yInModel <= 0.5){
+		// An intersection has occured, now check if we should update
+		if (rayInModel.intersection.none || t < ray.intersection.t_value){
+			// this unit square is the front most one to be intersected
+			// so we are update
+			ray.intersection.t_value = t;
+			ray.intersection.point = modelToWorld * Point3D(xInModel, yInModel, 0);
+			ray.intersection.normal = worldToModel.transpose() * Vector3D(0, 0, 1);
+			ray.intersection.none = false;
+			return true;
+		}
+	}
 	return false;
 }
 
